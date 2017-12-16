7137f1b1fde5f7cc48011775cc4faeacdcbf25ac
Shevchenko-ivan <shevchenkoivan1951@gmail.com>
2017-12-16 16:12:59 +0200

    add_JSON

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,10 @@
-    document.write("dvsv");
+$('document').ready(function(){
+    loadGoods();
+});
+
+function loadGoods(){
+        $.getJSON('goods.json', function(data){
+            console.log(data);
+        })
+    //Загружаю товари на сторінку
+}
