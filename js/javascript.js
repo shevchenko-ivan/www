ad0efbbd7c667824606276a512365939125a49ad
Shevchenko-ivan <shevchenkoivan1951@gmail.com>
2017-12-16 22:16:29 +0200

    fix

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,42 @@
+var cart = { }; //Моя корзина
+
 $('document').ready(function(){
     loadGoods();
+    checkCart();
 });
 
 function loadGoods(){
         $.getJSON('goods.json', function(data){
-            console.log(data);
-        })
-    //Загружаю товари на сторінку
+            var out = '';
+            for (var key in data){
+                out+='<div class="single-goods">';
+                out+='<img class="img-fluid img-thumbnail" src="'+data[key].image +'">';
+                out+='<p>'+data[key] ['name']+'</p>';
+                out+='<p>Ціна: '+data[key] ['cost']+'- грн</p>';
+                out+='<button type="button" class="add-to-cart btn btn-danger"data-art=" '+key+' ">ЗАМОВ</button>';
+                out+='</div>';
+            }
+            $('#goods').html(out);
+            $('button.add-to-cart').on('click', addToCart);
+        });
 }
+function addToCart(){
+    var articul = $(this).attr('data-art');
+    if ( cart[articul] !=undefined){
+         cart[articul]++;
+    }
+    else{
+        cart[articul] = 1;
+    }
+    localStorage.setItem('cart', JSON.stringify(cart));
+    console.log(cart);
+    //Добавлє товар в корзину
+}
+
+function checkCart(){
+    //провіряю наявність корзини в localStorage
+    if( localStorage.getItem('cart') != null){
+        cart = JSON.parse ( localStorage.getItem('cart'));
+    };
+}
+    //Загружаю товари на сторінку
